1affefd97a57574a8de743133bb0d01d0bf8efc5
a7ssan <44026213+a7ssan@users.noreply.github.com>
2018-12-24 13:41:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const client2 = new Discord.Client();
-
 const prefix = "!"
 
 client.on('ready', () => {
@@ -18,13 +17,13 @@ client.on('ready', () => {
 
 
 client.on('message', message => {
-    if(message.content === 's1'){
+    if(message.content === '-s1'){
         message.channel.send('#daily')
     }
 });
 
 client.on('message', message => {
-    if(message.content === 'ss1'){
+    if(message.content === '-ss1'){
         message.channel.send('#credits')
     }
 });
@@ -41,19 +40,19 @@ Client.on("message", async (message) => {
 });
 
 client2.on('message', message => {
-    if(message.content === 's2'){
+    if(message.content === '-s2'){
         message.channel.send('#daily')
     }
 });
 
 client2.on('message', message => {
-    if(message.content === 'ss2'){
+    if(message.content === '-ss2'){
         message.channel.send('#credits')
     }
 });
 
 Client2.on("message", async (message) => {
-	if (message.author.bot) return;
+	if (message.content) return;
 	if (!message.content.startsWith(prefix)) return;
 	
 	let command = message.content.split(" ")[0];
