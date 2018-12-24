22cdc93974a4f57862199a089a5228a6ce53269a
a7ssan <44026213+a7ssan@users.noreply.github.com>
2018-12-24 13:38:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const client2 = new Discord.Client();
 
+const prefix = "!"
+
 client.on('ready', () => {
    console.log(`----------------`);
    console.log(`Credit Farmm - Script By : Kahrbaa `);
@@ -16,38 +18,48 @@ client.on('ready', () => {
 
 
 client.on('message', message => {
-    if(message.content === '-s1'){
+    if(message.content === 's1'){
         message.channel.send('#daily')
     }
 });
 
 client.on('message', message => {
-    if(message.content === '-ss1'){
+    if(message.content === 'ss1'){
         message.channel.send('#credits')
     }
 });
 
-client.on('message', message => {
-    if(message.content === '-sss1'){
-        message.channel.send('#credits <@228108615976878080> 500')
+Client.on("message", async (message) => {
+	if (message.content) return;
+	if (!message.content.startsWith(prefix)) return;
+	
+	let command = message.content.split(" ")[0];
+	command = command.slice(prefix.length);
+	
+	let args = message.content.split(" ").slice(1);
     }
 });
 
 client2.on('message', message => {
-    if(message.content === '-s2'){
+    if(message.content === 's2'){
         message.channel.send('#daily')
     }
 });
 
 client2.on('message', message => {
-    if(message.content === '-ss2'){
+    if(message.content === 'ss2'){
         message.channel.send('#credits')
     }
 });
 
-client2.on('message', message => {
-    if(message.content === '-sss2'){
-        message.channel.send('#credits <@228108615976878080> 500')
+Client2.on("message", async (message) => {
+	if (message.author.bot) return;
+	if (!message.content.startsWith(prefix)) return;
+	
+	let command = message.content.split(" ")[0];
+	command = command.slice(prefix.length);
+	
+	let args = message.content.split(" ").slice(1);
     }
 });
 
